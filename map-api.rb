cd7c6e69514e65662e9a10102dcfa1a71c4cbb2f
Maricel Dragan <mariaceliadragan@gmail.com>
2022-10-04 17:15:32 +0100

    changes map

--- map-api.rb
+++ map-api.rb
@@ -1,15 +1,15 @@
 require "uri"
 require "net/http"
 require "json"
+require "./index.js"
 
 
+# url = URI("https://maps.googleapis.com/maps/api/js?key=AIzaSyBpGH4cWpYz5cAYIyN01D9b0bBbszawKRw&callback=initMap")
 
-url = URI("https://maps.googleapis.com/maps/api/js?key=AIzaSyBpGH4cWpYz5cAYIyN01D9b0bBbszawKRw&callback=initMap")
+# https = Net::HTTP.new(url.host, url.port)
+# https.use_ssl = true
 
-https = Net::HTTP.new(url.host, url.port)
-https.use_ssl = true
+# request = Net::HTTP::Get.new(url)
 
-request = Net::HTTP::Get.new(url)
-
-response = https.request(request)
-puts response.read_body
+# response = https.request(request)
+# puts response.read_body
--- views/index.js
+++ views/index.js
@@ -1,7 +1,7 @@
-function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
-      center: {lat: -34.397, lng: 150.644},
-      zoom: 8,
-      mapId: 'ca5dec382f52fe24'
-});
-}
+// function initMap() {
+//     map = new google.maps.Map(document.getElementById('map'), {
+//       center: {lat: -34.397, lng: 150.644},
+//       zoom: 8,
+//       mapId: 'ca5dec382f52fe24'
+// });
+// }
