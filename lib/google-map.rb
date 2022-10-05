93b743bdb304e81aaa99075aba741f44525e33be
Maricel Dragan <mariaceliadragan@gmail.com>
2022-10-05 17:12:51 +0100

    small

--- lib/google-map.rb
+++ lib/google-map.rb
@@ -2,14 +2,14 @@ require 'google_maps_service'
 require_relative '../.api_key'
 
 class MapFinder 
-    def initialize(map)
+    def initialize
      puts $api_key 
     # Setup API keys
      @gmaps = GoogleMapsService::Client.new(key: $api_key)
   end
     
     def showMap
-      geocode_result = @maps.geocode(address: ‘London’)
+      geocode_result = @gmaps
     #   puts geocode_result.first[:types]
         return geocode_result
     
