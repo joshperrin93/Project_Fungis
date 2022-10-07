c077eed218ce53aff15f1a643adb8892306b1cf1
Ella Bastian <ellabastian@hotmail.co.uk>
2022-10-07 18:11:01 +0100

    Test for find name method

---
+++ spec/restaurant_finder_spec.rb
@@ -0,0 +1,19 @@
+RSpec.describe "RestaurantFinder" do
+    # it "calls the Google Places API and returns info about the first place" do
+    #     place2 = double('place')
+
+    #     allow(place2).to receive(:item) {"\"WAVE\", \"11 Dispensary Ln, London E8 1FT, United Kingdom\", 4.7, [#<GooglePlaces::Photo:0x000000...zaSyDSDGq-UtxFetovCVYSqyEAT9VreL5IEEQ\">], \"ChIJi5rjorccdkgRhHRpRo5pQDE\", 51.5240081, -0.0727076"}
+
+    #     places = RestaurantFinder.new("London", " ")
+    #     expect(places.find.first).to eq(["WAVE", "11 Dispensary Ln, London E8 1FT, United Kingdom", 4.7, [#<GooglePlaces::Photo:0x00000001074...yDSDGq-UtxFetovCVYSqyEAT9VreL5IEEQ">], "ChIJF_kxdtYddkgR2ljex8ywClg", 51.54887739999999, -0.0546374])
+    # end
+    it "calls the Google Places API and returns the name of the first place" do
+        place2 = double('place')
+
+        allow(place2).to receive(:item) {'WAVE'}
+
+        places = RestaurantFinder.new("London", " ")
+        expect(places.find_name.first).to eq(['WAVE'])
+        end 
+end
+end
