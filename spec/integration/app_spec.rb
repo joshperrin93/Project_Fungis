e78b796baf220a78034da851e7e510f5e21e667c
Ella Bastian <ellabastian@hotmail.co.uk>
2022-10-07 19:00:38 +0100

    Added more tests

--- spec/integration/app_spec.rb
+++ spec/integration/app_spec.rb
@@ -19,10 +19,35 @@ describe Application do
     # end
     
     context "GET /index" do
-        it 'returns the search page' do
+        it 'returns 200 OK and the search page' do
         response = get('/index')
         expect(response.status).to eq(200)
         expect(response.body).to include('<h1> Fungis </h1>')
         end
     end
+
+    context "POST /index" do
+        it "doesn't log a user in if they input incorrect credentials" do
+            response = post("/index", email: "ella@makers.com", password: "assword!123")
+            expect(response.status).to eq 200
+            expect(response.body).to include('<h1>Incorrect credentials</h1>')
+        end
+        it "does log a user in if they input correct credentials" do
+            response = post("/index", email: "ella@makers.com", password: "password!123")
+            expect(response.status).to eq 302
+            expect(response.body).to include('')
+        end
+    end
+
+    context 'GET /signup_success' do
+        xit "lets a user know that they have signed up successfully" do
+            response = get('/signup_success')
+            expect(response.status).to eq 200
+            expect(response.body).to include('<h1>Welcome to Fungis <%=@new_user.name%>!</h1>')
+        end
+    end
+
+
+
+
 end
