403ba654b505ec79c00fc35db5944f51bee35b02
kuwotu <kevwe1996@gmail.com>
2022-10-10 17:24:57 +0100

    favourite function changed

--- lib/favorites_repository.rb
+++ lib/favorites_repository.rb
@@ -19,20 +19,44 @@ class Favorites_Repository
     return @favorites
     end
 
+    def user_favorite(user_session_id)
+      @favorites = []
+      
+      sql = 'SELECT * FROM favourites WHERE user_id = $1;'
+      sql_params = [user_session_id]
+      result_set = DatabaseConnection.exec_params(sql, sql_params)
+      result_set.each do |record|
+        favorite = Favorites.new
+        favorite.id = record['id'].to_i
+        favorite.place_id = record['place_id']
+        favorite.user_id = record['user_id']
+        favorite.name= record['name']
+        @favorites << favorite
+    end
+    return @favorites
+    end
+
 
     def create(favorite)
-      sql = 'INSERT INTO favourites (place_id, user_id, name)
-                VALUES($1, $2, $3);
-            '
+      sql = 'SELECT * FROM favourites WHERE place_id = $1 AND user_id =$2;'
       sql_params = [
+        favorite.place_id,
+        favorite.user_id
+      ]
+      result_set = DatabaseConnection.exec_params(sql, sql_params)
+      if (result_set.any?)
+      return false
+       else 
+        sql = 'INSERT INTO favourites (place_id, user_id, name)
+        VALUES($1, $2, $3);'
+        sql_params = [
         favorite.place_id,
         favorite.user_id,
         favorite.name
       ]
-     p sql_params 
-     
-  
       result_set = DatabaseConnection.exec_params(sql, sql_params)
+       end
+      
     end
 
 end
